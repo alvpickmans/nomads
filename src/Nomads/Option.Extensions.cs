5d25a32afb36c73a924e747552f91b7b36ae690d
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2023-09-02 15:10:15 +0200

    Option documetation

--- src/Nomads/Option.Extensions.cs
+++ src/Nomads/Option.Extensions.cs
@@ -1,12 +1,47 @@
 namespace Nomads;
 
+/// <summary>
+/// Extension methods for <see cref="Option{T}"/>
+/// </summary>
 public static class Option
 {
+    /// <summary>
+    /// Creates a new instance of <see cref="Option{T}"/> with a value
+    /// </summary>
+    /// <param name="value">Option's value</param>
+    /// <typeparam name="T">Type of value</typeparam>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// Option<string> stringOption = Some("Hello");
+    /// Option<bool> booleanOption = Some(true);
+    /// ]]>
+    /// </code>
+    /// </example>
+    /// <returns></returns>
     public static Option<T> Some<T>(T value) where T : notnull => new(value);
 
+    /// <summary>
+    /// Creates a new instance of <see cref="None"/>, generally used to convert it to
+    /// an empty <see cref="Option{T}"/>
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// Option<string> stringOption = None();
+    /// Option<bool> booleanOption = None();
+    /// ]]>
+    /// </code>
+    /// </example>
+    /// <returns></returns>
     public static None None() => new();
     
-    public static Option<T> None<T>() where T : notnull => new(default);
-
+    /// <summary>
+    /// Overload of <see cref="Option{T}.HasValue"/> that does not return the
+    /// value of <see cref="T"/> in case of non-empty Option.
+    /// </summary>
+    /// <param name="option">Option</param>
+    /// <typeparam name="T">Type of option value</typeparam>
+    /// <returns></returns>
     public static bool HasValue<T>(this Option<T> option) where T : notnull => option.HasValue(out _);
 }
--- src/Nomads/Option.cs
+++ src/Nomads/Option.cs
@@ -13,14 +13,32 @@ public readonly struct Option<T> where T : notnull
     private readonly T? _value;
     private readonly bool _hasValue;
 
+    /// <summary>
+    /// Creates a instances of <see cref="Option{T}"/> with a value
+    /// </summary>
+    /// <param name="value"></param>
     internal Option(T value) => (_value, _hasValue) = (value, true);
 
+    /// <summary>
+    /// Implicitly creates an instance of <see cref="Option{T}"/> withy a value
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
     public static implicit operator Option<T>(T value) => new(value);
     
+    /// <summary>
+    /// Implicitly creates an empty instance of <see cref="Option{T}"/>
+    /// </summary>
+    /// <param name="_"></param>
+    /// <returns></returns>
     public static implicit operator Option<T>(None _) => new();
 
-    public static T operator !(Option<T> option) => option._value!;
-
+    /// <summary>
+    /// Determines if the <see cref="Option{T}"/> has a value,
+    /// returning it if so.
+    /// </summary>
+    /// <param name="value">Possible value if the option is not empty</param>
+    /// <returns>True if the option has a value, false if not</returns>
     public bool HasValue(
         #if NET_6_OR_GREATER
         [NotNullWhen(true)]
