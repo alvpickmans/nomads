e7f26c59bc3f48dfec55b9952a6f102bc9b17e39
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2023-09-02 13:43:06 +0200

    update options internals

--- src/Nomads/Option.cs
+++ src/Nomads/Option.cs
@@ -4,15 +4,22 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Nomads;
 
+/// <summary>
+/// Base model (functor?) representing either a value or nothing.
+/// </summary>
+/// <typeparam name="T">Type of value</typeparam>
 public readonly struct Option<T> where T : notnull
 {
     private readonly T? _value;
+    private readonly bool _hasValue;
 
-    internal Option(T? value) => _value = value;
+    internal Option(T value) => (_value, _hasValue) = (value, true);
 
     public static implicit operator Option<T>(T value) => new(value);
     
-    public static implicit operator Option<T>(None none) => new(default);
+    public static implicit operator Option<T>(None _) => new();
+
+    public static T operator !(Option<T> option) => option._value!;
 
     public bool HasValue(
         #if NET_6_OR_GREATER
@@ -21,6 +28,6 @@ public readonly struct Option<T> where T : notnull
         out T? value)
     {
         value = _value;
-        return value is not null;
+        return _hasValue;
     }
 }
