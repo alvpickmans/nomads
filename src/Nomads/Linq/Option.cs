22db26601b82caa9019cb5e282cc1b91e32ed3e4
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2023-09-03 18:12:19 +0200

    linq xml docs

--- src/Nomads/Linq/Option.cs
+++ src/Nomads/Linq/Option.cs
@@ -2,8 +2,19 @@ using static Nomads.Option;
 
 namespace Nomads.Linq;
 
+/// <summary>
+/// Linq-like extension methods for <see cref="Option{T}"/>
+/// </summary>
 public static class Option
 {
+    /// <summary>
+    /// Applies a mapping function to a <see cref="Option{T}"/>
+    /// </summary>
+    /// <param name="option">Input option</param>
+    /// <param name="selector">Mapping function</param>
+    /// <typeparam name="TIn">Source type</typeparam>
+    /// <typeparam name="TOut">Target type</typeparam>
+    /// <returns>An <see cref="Option{T}"/> with its value type mapped</returns>
     public static Option<TOut> Select<TIn, TOut>(
         this Option<TIn> option,
         Func<TIn, TOut> selector)
@@ -13,6 +24,14 @@ public static class Option
             ? selector.Invoke(option.Value!)
             : None();
     
+    /// <summary>
+    /// Applies a mapping function to a <see cref="Option{T}"/>
+    /// </summary>
+    /// <param name="option">Input option</param>
+    /// <param name="selector">Mapping function</param>
+    /// <typeparam name="TIn">Source type</typeparam>
+    /// <typeparam name="TOut">Target type</typeparam>
+    /// <returns>An <see cref="Option{T}"/> with its value type mapped</returns>
     public static Option<TOut> Select<TIn, TOut>(
         this Option<TIn> option,
         Func<TIn, Option<TOut>> selector)
--- src/Nomads/Linq/Result.cs
+++ src/Nomads/Linq/Result.cs
@@ -2,8 +2,20 @@ using static Nomads.Result;
 
 namespace Nomads.Linq;
 
+/// <summary>
+/// Linq-like extension methods for <see cref="Result{TValue,TError}"/>
+/// </summary>
 public static class Result
 {
+    /// <summary>
+    /// Applies a mapping function to a <see cref="Result{TValue,TError}"/>
+    /// </summary>
+    /// <param name="result">Input result</param>
+    /// <param name="selector">Mapping function</param>
+    /// <typeparam name="TIn">Source type</typeparam>
+    /// <typeparam name="TOut">Target type</typeparam>
+    /// <typeparam name="TError">Error type</typeparam>
+    /// <returns>A <see cref="Result{TValue,TError}"/> with its value type mapped</returns>
     public static Result<TOut, TError> Select<TIn, TOut, TError>(
         this Result<TIn, TError> result,
         Func<TIn, TOut> selector)
@@ -14,6 +26,15 @@ public static class Result
             ? Ok(selector.Invoke(result.Value!))
             : Error(result.Error!);
     
+    /// <summary>
+    /// Applies a mapping function to a <see cref="Result{TValue,TError}"/>
+    /// </summary>
+    /// <param name="result">Input result</param>
+    /// <param name="selector">Mapping function</param>
+    /// <typeparam name="TIn">Source type</typeparam>
+    /// <typeparam name="TOut">Target type</typeparam>
+    /// <typeparam name="TError">Error type</typeparam>
+    /// <returns>A <see cref="Result{TValue,TError}"/> with its value type mapped</returns>
     public static Result<TOut, TError> Select<TIn, TOut, TError>(
         this Result<TIn, TError> result,
         Func<TIn, Result<TOut, TError>> selector)
